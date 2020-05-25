0c445c1fcf7f645b5e9b2a692e61ac7338b7d956
willem-mbp <willem@itecho.co.za>
2020-05-25 14:08:20 +0200

    add puppeteer event

--- node/pup.js
+++ node/pup.js
@@ -1,61 +1,68 @@
 const puppeteer = require('puppeteer');
-const compiler = require('vue-template-compiler');
-
-const htmlContent = `
-  <!doctype html>
-  <html>
-    <head><meta charset='UTF-8'><title>Test</title></head>
-    <body>
-    <div v-data="getData()">Test</div>
-    
-    <script>
-        function getData() {
-            return {count: 10};
-        }
-    </script>
-
-    </body>
-  </html>
-`;
 
 puppeteer.launch({
-    headless: false
+    headless: true
 }).then(async (browser) => {
     const page = await browser.newPage();
-    await page.setContent(htmlContent);
-    console.log(await page.content());
+    
+    var ret = new Promise(async (res, rej) => {
+        // Expose a handler to the page
+        var timer = null;
+        await page.exposeFunction('renderEvent', (id, content) => {
+            console.log(`Event fired: ${id}, htmlContent: ${content}`);
+            // cancel the timer
+            clearTimeout(timer);
+            res({id, content});
+        });
+
+        timer = setTimeout(() => {
+            console.log('Timer done');
+            rej();
+        }, 2000);
+        await page.goto('http://localhost:8080');
+    });
+
+    var res = await ret;
+    console.log('Result', res);
+    await browser.close();
+    
+    // await page.setContent(htmlContent);
+    
+    // console.log(await page.content());
+
 
     // Get the "viewport" of the page, as reported by the page.
-  const list = await page.evaluate(() => {
+    //     const list = await page.evaluate(() => {
 
-        var ret = [];
-        var list = document.querySelectorAll('[v-data]');
-        for (var i = 0; i < list.length; i++) {
-            var l = list[i];
-            if (l.nodeType == Node.ELEMENT_NODE) {
-                var value = l.attributes.getNamedItem("v-data").value;
-                // l.attributes.removeNamedItem('v-data');
-                
-                var data;
-                try {
-                    data = (new Function('return ' + value)).call(window);
-                    console.log('Data function', data);
-                }
-                catch (err) {
-                    console.error('Could not execute v-data expression. ', err);
-                }
+    //         var ret = [];
+    //         var list = document.querySelectorAll('[v-data]');
+    //         for (var i = 0; i < list.length; i++) {
+    //             var l = list[i];
+    //             if (l.nodeType == Node.ELEMENT_NODE) {
+    //                 var value = l.attributes.getNamedItem("v-data").value;
+    //                 // l.attributes.removeNamedItem('v-data');
 
-                ret.push([value, l.outerHTML, data]);
+    //                 var data;
+    //                 try {
+    //                     data = (new Function('return ' + value)).call(window);
+    //                     console.log('Data function', data);
+    //                 }
+    //                 catch (err) {
+    //                     console.error('Could not execute v-data expression. ', err);
+    //                 }
 
-            }
-        }
-        return ret;
-  });
+    //                 ret.push([value, l.outerHTML, data]);
 
-  var com = compiler.compile(list[0][1]);
-  console.log('com:', com);
-  console.log('list:', list);
+    //             }
+    //         }
+    //         return ret;
+    //   });
 
-  await browser.close();
+    //   var com = compiler.compile(list[0][1]);
+    //   console.log('com:', com);
+    //   console.log('list:', list);
 
+
+    
+    
 });
