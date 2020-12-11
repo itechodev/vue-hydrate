4218b043018dfa325c7cf033e355113615f5ef84
Willem Bijker <willem@itecho.co.za>
2020-12-11 16:44:05 +0200

    Simplify route matching

--- web/router.js
+++ web/router.js
@@ -1,5 +1,12 @@
 import Vue from 'vue/dist/vue.common.js';
 
+
+const vm = new Vue ({
+    data: {
+        path: null
+    }
+});
+
 const routeComponents = [];
 
 window.onpopstate = function(event) {
@@ -7,36 +14,25 @@ window.onpopstate = function(event) {
 }
 
 export default function refreshRouters() {
+    vm.path = document.location.pathname;
+    // data.path =
     for (const r of routeComponents) {
         r.show = r.url === document.location.pathname;
     }
 }
 
-Vue.component('v-route', {
-    props: {
-        url: String
-    },
-    created() {
-        routeComponents.push(this);
-    },
-    beforeDestroy() {
-        routeComponents.splice(routeComponents.indexOf(this), 1)
-    },
-    data() {
-        return { show: false}
-    },
-    render(h) {
-        if (this.show)
-            return h('div', this.$slots.default);
-
-        return null;
-    }
-});
-
-
 Vue.prototype.$router = {
+
+    get path() {
+        return vm.path;
+    },
+
     push(url, data) {
         window.history.pushState(data, url, url);
         refreshRouters();
+    },
+
+    match(path) {
+        return this.path === path;
     }
 }
