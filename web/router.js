c08106be1ed120117fbadead6a71b2dfdc1727d0
willem-mbp <willem@itecho.co.za>
2020-12-12 11:05:48 +0200

    Router params

--- web/router.js
+++ web/router.js
@@ -6,15 +6,13 @@ export default {
         // Other way is to use a global mixin, but then you'll have to use $data.$route
         const vm = new Vue({
             data: {
-                path: null
+                path: null,
+                params: {}
             }
         });
 
         function refresh() {
             vm.path = document.location.pathname;
-
-            // /book/12 match /:book/:id
-
         }
 
         window.onpopstate = refresh;
@@ -26,14 +24,31 @@ export default {
                 return vm.path;
             },
 
+            get params() {
+                return vm.params;
+            },
+
             push(url, data) {
                 window.history.pushState(data, url, url);
                 refresh();
             },
 
             match(path) {
-                // convert /:book/:id to regex /\/(\w+)\/(\w+)/
+                // reverse order
+                let seg = [];
+                for (let p of [...path.matchAll(/:(\w+)/g)].reverse()) {
+                    path = path.substr(0, p.index) + "(\\w+)" + path.substr(p.index + p[0].length);
+                    seg.unshift(p[1]);
+                }
 
+                let match = this.path.match(new RegExp(path));
+                if (match != null) {
+                    const newSet = {};
+                    seg.forEach((s, i) => {
+                        newSet[s] = match[i + 1];
+                    });
+                    vm.params = newSet;
+                }
                 return false;
             }
         }
